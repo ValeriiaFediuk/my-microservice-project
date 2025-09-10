f4d908aaeb7a71bcbb5c5a238e42fd82c821cbbe
ValeriiaFediuk <fediuk.valeriia14@gmail.com>
2025-09-10 22:03:29 +0300

    first trial

--- lesson-5/outputs.tf
+++ lesson-5/outputs.tf
@@ -27,3 +27,14 @@ output "internet_gateway_id" {
 output "ecr_repository_url" {
   value = module.ecr.repository_url
 }
+
+output "eks_cluster_endpoint" {
+  description = "EKS API endpoint for connecting to the cluster"
+  value       = module.eks.eks_cluster_endpoint
+}
+output "eks_cluster_name" {
+  value       = module.eks.eks_cluster_name
+}
+output "eks_node_role_arn" {
+  value       = module.eks.eks_node_role_arn
+}
